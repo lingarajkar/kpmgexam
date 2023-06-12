bfe6f3538dabbe04a64a390d863345f37f85cfed
Lingaraj Kar <lingaraj.kar@globallogic.com>
2023-06-12 10:56:58 +0530

    changing the user data of ec2

--- Question-1/envs/prod/modules/ec2/install.sh
+++ Question-1/envs/prod/modules/ec2/install.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
-#VM Startup Script
-sudo yum update -y
-sudo amazon-linux-extras install docker -y
-sudo systemctl enable docker --now
-sudo systemctl enable containerd.service --now
-sudo usermod -a -G docker ec2-user
-sudo docker pull bjwrd/app.py
-sudo docker run -d bjwrd/app.py 
+yum update -y
+yum install -y httpd
+systemctl start httpd.service
+systemctl enable httpd.service
+echo "Hello World" > /var/www/html/index.html
